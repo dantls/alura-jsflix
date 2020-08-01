dfdbd148e6f0a66aa3024f15af493723da86cfef
dantls <dan_tls@hotmail.com>
2020-08-01 10:57:51 -0400

    'Beginning-of-class-3

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import PageDefault from '../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 const Category = () => {
+  const [category, setCategory] = useState('Filmes');
+  console.log(category)
+
   return (
     <PageDefault>
-      <h1>Cadastro de categoria</h1>
+      <h1>Cadastro de categoria : {category} </h1>
       <Link to="/">
         Voltar para Home
       </Link>
